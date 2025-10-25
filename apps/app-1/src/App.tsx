898d52ffb40c90e967e0fa6bdb038d10118138a3
Jussi Heikkinen <jussih91@gmail.com>
2025-10-25 08:51:00 +0300

    chore: use component library

--- apps/app-1/src/App.tsx
+++ apps/app-1/src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import { Button } from 'component-library';
 
 const App = () => {
-  return <div className="content">App 1</div>;
+  return (
+    <div className="content">
+      <h2>App 1</h2>
+      <Button variant="default">Click me!</Button>
+    </div>
+  );
 };
 
 export default App;
--- apps/app-1/src/bootstrap.tsx
+++ apps/app-1/src/bootstrap.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import 'component-library/styles';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
